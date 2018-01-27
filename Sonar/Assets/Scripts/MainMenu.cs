18b1ace5cd0776492af5ac3e268f2901f9f3dfd1
Joshua Stankiewicz <shotgunjesus@gmail.com>
2018-01-27 11:31:42 -0600

    Hopefully this doesn't break anything. Wish me luck!

---
+++ Sonar/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string SceneToLoad = "Scene1";
+
+    // OnClick function for "Play" button
+    public void StartGame()
+    {
+        Debug.Log("Start the game!");
+        SceneManager.LoadScene(SceneToLoad);
+    }
+
+    // OnClick function for "Quit" button
+    public void QuitGame()
+    {
+        Debug.Log("Game should be closing now.");
+        Application.Quit();
+    }
+}
