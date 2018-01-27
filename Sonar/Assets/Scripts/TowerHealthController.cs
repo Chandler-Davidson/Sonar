8c1608a31a5f278e245c554668fdec4e7adde8ab
Joshua Stankiewicz <shotgunjesus@gmail.com>
2018-01-27 13:50:18 -0600

    Added Beacon effects

--- Sonar/Assets/Scripts/TowerHealthController.cs
+++ Sonar/Assets/Scripts/TowerHealthController.cs
@@ -6,9 +6,29 @@ public class TowerHealthController : MonoBehaviour {
     public int towerDataLeft;
     private bool towerAlive;
 
+    // sprite array for different power up levels
+    public Sprite[] BeaconPhases = new Sprite[6];
+    private SpriteRenderer spriteRenderer;
+    private int spriteNumber = 0;
+
+    // emit effect for when hit
+    public GameObject HitEffect;
+    public Transform EffectSpawnPoint;
+    public AudioClip EffectSound;
+    private AudioSource dj;
+
 	// Use this for initialization
 	void Start () {
         towerAlive = true;
+
+        dj = FindObjectOfType<AudioSource>();
+
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+
+        if (HitEffect == null)
+        {
+            HitEffect = GameObject.Find("BeaconEffect");
+        }
 	}
 
     // Update is called once per frame
@@ -21,6 +41,13 @@ public class TowerHealthController : MonoBehaviour {
         if (towerAlive && collision.gameObject.tag == "Sonar") {
             towerDataLeft--;
 
+            dj.PlayOneShot(EffectSound);
+
+            GameObject killThis = Instantiate(HitEffect, EffectSpawnPoint.position, Quaternion.identity);
+            Destroy(killThis, 0.5f);
+
+            spriteRenderer.sprite = BeaconPhases[++spriteNumber];
+
             if (towerDataLeft <= 0) {
                 TowerComplete();
             }
@@ -29,6 +56,7 @@ public class TowerHealthController : MonoBehaviour {
 
     private void TowerComplete() {
         towerAlive = false;
+        spriteRenderer.sprite = BeaconPhases[BeaconPhases.Length - 1];
         print("TOWER COMPLETE");
     }
 }
---
+++ Sonar/Assets/Scripts/TowerManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerManager : MonoBehaviour
+{
+    public GameObject[] towers = new GameObject[3];
+    public GameObject wall;
+
+    bool[] towersOnline;
+
+	// Use this for initialization
+	void Start ()
+    {
+        towersOnline = new bool[towers.Length];
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+}
