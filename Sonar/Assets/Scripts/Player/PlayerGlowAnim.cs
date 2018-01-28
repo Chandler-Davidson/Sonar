27b1d4ff83eb2b94571902e4c41c84fcc00c52d8
Joshua Stankiewicz <shotgunjesus@gmail.com>
2018-01-28 12:50:18 -0600

    Player animation sprite inserted.

---
+++ Sonar/Assets/Scripts/Player/PlayerGlowAnim.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGlowAnim : MonoBehaviour
+{
+    Animator anim;
+    public AnimationClip idleClip;
+    public AnimationClip fireClip;
+
+    float delta = 0.0f;
+    float duration;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+        duration = fireClip.length;
+        anim.Play(idleClip.name);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (delta > 0.0f)
+        {
+            delta -= Time.deltaTime;
+            if (delta <= 0.0f)
+            {
+                anim.Play(idleClip.name);
+            }
+        }
+	}
+
+    public void Animate()
+    {
+        anim.Play(fireClip.name);
+        delta = duration;
+    }
+}
