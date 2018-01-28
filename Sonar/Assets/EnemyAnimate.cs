f949653ffd14853ef17e44bdad26b521a69b2fe1
Joshua Stankiewicz <shotgunjesus@gmail.com>
2018-01-28 17:50:38 -0600

    Everything as published initially to the jam site

---
+++ Sonar/Assets/EnemyAnimate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimate : MonoBehaviour
+{
+    Animator anim;
+    public AnimationClip clip;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+        anim.Play(clip.name);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
